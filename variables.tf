a4dfbd5e35d55d5e514c88ca6d389e5493f80ba4
Paulo Oliveira <paulltecno@gmail.com>
2024-05-04 20:45:03 +0000

    Add: Network terraform iac files

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  type        = string
+  description = "Nome da sua VPC"
+}
+
+variable "vpc_range" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1a_range" {
+  type    = string
+  default = "10.0.10.0/24"
+}
+
+variable "public_subnet_1c_range" {
+  type    = string
+  default = "10.0.20.0/24"
+}
+
+variable "private_subnet_1a_range" {
+  type    = string
+  default = "10.0.100.0/24"
+}
+
+variable "private_subnet_1c_range" {
+  type    = string
+  default = "10.0.200.0/24"
+}
